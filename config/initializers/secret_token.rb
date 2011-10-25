a5e2a9c2aae8d1762d06230729da0292bc2c5fad
Johan Andersson <johan.rejeep@gmail.com>
2011-10-25 09:23:23 +0200

    Added Rails files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bsboll::Application.config.secret_token = '14b6273f6d13e62db9ca0957b0a93fbdb995d45e9f254bac19056dcf5bc7e3dd92c79f0130e9ed80fcfd0184f0791b2ddf6c1b566495a4d4f3bc2ff93fa6ad3d'

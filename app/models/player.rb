bfd22ef24cdc65baca7f8d3791ce9aeba8f54c03
Johan Andersson <johan.rejeep@gmail.com>
2011-10-26 11:19:05 +0200

    Player must have first and last name.

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  
+  validates :first_name, :last_name, :presence => true
 end
